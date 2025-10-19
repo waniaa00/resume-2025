4a2182431d30804663b228c7ebb447fc7dbba3ad
wania akram <160944972+waniaa00@users.noreply.github.com>
2025-10-19 15:35:40 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,8 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
+  title: 'resume-2025',
+  description: 'W. resume',
   generator: 'v0.app',
 }
 
